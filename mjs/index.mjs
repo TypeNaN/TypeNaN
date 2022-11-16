172c9eb362c76fbc89e5fc9ba6c6c34fafaaa73b
TypeNaN <krajaymail@gmail.com>
2022-11-16 13:44:44 +0700

    shorten the code

--- mjs/index.mjs
+++ mjs/index.mjs
@@ -3,17 +3,13 @@
 
 document.addEventListener('DOMContentLoaded', async () => {
   let conf = JSON.parse(sessionStorage.getItem('conf')) || { lang: false, viewer: false }
-  if (conf['lang'] && conf['viewer']) {
-    document.documentElement.lang = conf['lang']
-    const module = await import(`./${conf['viewer']}.mjs`)
-    new module.default()
-  } else {
+  if (!conf['lang'] || !conf['viewer']) {
     const landing = await import('./landing.mjs')
     const land = new landing.default()
     await land.render()
     conf = land.getconf()
-    document.documentElement.lang = conf['lang']
-    const module = await import(`./${conf['viewer']}.mjs`)
-    new module.default()
   }
+  document.documentElement.lang = conf['lang']
+  const module = await import(`./${conf['viewer']}.mjs`)
+  new module.default()
 })
